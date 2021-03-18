b0d760b9d8255ec806e0d5f068085ae4cef9e372
f1se4 <52441535+f1se4@users.noreply.github.com>
2021-03-18 10:07:49 +0100

    chartist.js + char.js (web only)

--- Contents/Resources/modules/customjs.js
+++ Contents/Resources/modules/customjs.js
@@ -22,6 +22,28 @@
 
 window.addEventListener('load', function() {
 
+    var processCharlist = function() {
+        var elements = document.getElementsByClassName('chartist')
+        for (var i = 0, l = elements.length; i < l; i++) {
+            // only get the first one in the array, because the previous one has been removed or replaced
+            var element = elements[0]
+            var source_raw = element.textContent.replace(/(['"])?([a-zA-Z0-9_]+)(['"])?:/g, '"$2": ');
+
+            var source = JSON.parse(source_raw)
+            console.log(source)
+            // Create a new div for displaying chart
+            var div_new = document.createElement('div')
+            var div_new_id = 'chartist_' + i
+            var dot_new_id = '.chartist_' + i
+            div_new.setAttribute('id', div_new_id)
+            div_new.setAttribute('class', div_new_id)
+            element.parentNode.replaceChild(div_new, element)
+
+            //Generate Graphic
+            new Chartist.Bar(dot_new_id, source);     
+        }   
+    }
+
     var processChart = function() {
         var elements = document.getElementsByClassName('chartgraf')
         for (var i = 0, l = elements.length; i < l; i++) {
@@ -36,8 +58,8 @@ window.addEventListener('load', function() {
             element.parentNode.replaceChild(can_new, element)
 
             //Generate Graphic
-            var ctx = document.getElementById('myChart').getContext('2d');
-            var mychart = new Chart(ctx, JSON.parse(source.trim()));
+            var ctx = document.getElementById(can_new_id).getContext('2d');
+            mychart = new Chart(ctx, JSON.parse(source.trim()));
         }
     }
     var processFlowchart = function() {
@@ -62,6 +84,7 @@ window.addEventListener('load', function() {
     var refresh = function() {
         processFlowchart()
         processChart()
+        processCharlist()
     }
     //refresh()
     document.body.addEventListener('ia-writer-change', refresh)
